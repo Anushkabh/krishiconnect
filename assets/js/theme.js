72a18e4246b0969210310f953cb864717d3a4164
srivastav2006 <srivastav3306@gmail.com>
2024-05-15 15:35:48 +0530

    Logo design and navbar addition to other pages where nav bar is absent

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -8,9 +8,13 @@ $(document).ready(function(){
         if (scroll > 200) {
             $('#page-navigation').addClass('bg-white').removeClass('bg-transparent');
             $('#page-navigation').addClass('navbar-light').removeClass('navbar-dark');
+            $('#your-image').attr('src', 'assets/img/logo/krishiconnect-high-resolution-logo.png'); 
+        
         }else{
             $('#page-navigation').addClass('bg-transparent').removeClass('bg-white');
             $('#page-navigation').addClass('navbar-dark').removeClass('navbar-light');
+            $('#your-image').attr('src', 'assets/img/logo/krishiconnect-high-resolution-logo_black.png'); 
+            
         }
     });
 
