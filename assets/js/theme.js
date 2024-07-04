7c7f960e3645b6c0d4798cfcc38f040cbbced49e
Himanshu Agarwal <himanshuaaagarwal2002@gmail.com>
2024-07-04 15:48:46 +0530

    Added prettier for formatting codes

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,121 +1,130 @@
-$(document).ready(function(){
-    $(function () {
-        $(".preloader").fadeOut();
-    });
+$(document).ready(function () {
+  $(function () {
+    $('.preloader').fadeOut();
+  });
 
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 200) {
-            $('#page-navigation').addClass('bg-white').removeClass('bg-transparent');
-            $('#page-navigation').addClass('navbar-light').removeClass('navbar-dark');
-            $('#your-image').attr('src', 'assets/img/logo/krishiconnect-high-resolution-logo.png'); 
-        
-        }else{
-            $('#page-navigation').addClass('bg-transparent').removeClass('bg-white');
-            $('#page-navigation').addClass('navbar-dark').removeClass('navbar-light');
-            $('#your-image').attr('src', 'assets/img/logo/krishiconnect-high-resolution-logo_black.png'); 
-            
-        }
-    });
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 200) {
+      $('#page-navigation').addClass('bg-white').removeClass('bg-transparent');
+      $('#page-navigation').addClass('navbar-light').removeClass('navbar-dark');
+      $('#your-image').attr(
+        'src',
+        'assets/img/logo/krishiconnect-high-resolution-logo.png'
+      );
+    } else {
+      $('#page-navigation').addClass('bg-transparent').removeClass('bg-white');
+      $('#page-navigation').addClass('navbar-dark').removeClass('navbar-light');
+      $('#your-image').attr(
+        'src',
+        'assets/img/logo/krishiconnect-high-resolution-logo_black.png'
+      );
+    }
+  });
 
-    $('.landing-categories').owlCarousel({
-        loop: true,
+  $('.landing-categories').owlCarousel({
+    loop: true,
+    items: 4,
+    margin: 0,
+    autoplay: true,
+    dots: false,
+    autoplayTimeout: 8000,
+    rewindSpeed: 8000,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      990: {
         items: 4,
-        margin: 0,
-        autoplay: true,
-        dots:false,
-        autoplayTimeout: 8000,
-        rewindSpeed : 8000,
-        nav: true,
-        navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            990: {
-                items: 4
-            },
-            1170: {
-                items: 4
-            }
-        }
-
-    });
-
-    $('.shop-categories').owlCarousel({
-        loop: true,
+      },
+      1170: {
         items: 4,
-        margin: 5,
-        autoplay: false,
-        dots:false,
-        autoplayTimeout: 8000,
-        rewindSpeed : 8000,
-        nav: true,
-        navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            990: {
-                items: 4
-            },
-            1170: {
-                items: 4
-            }
-        }
+      },
+    },
+  });
 
-    });
-
-    $('.product-carousel').owlCarousel({
-        loop: true,
+  $('.shop-categories').owlCarousel({
+    loop: true,
+    items: 4,
+    margin: 5,
+    autoplay: false,
+    dots: false,
+    autoplayTimeout: 8000,
+    rewindSpeed: 8000,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      990: {
         items: 4,
-        margin: 15,
-        autoplay: false,
-        dots:false,
-        autoplayTimeout: 8000,
-        rewindSpeed : 8000,
-        nav: true,
-        navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            990: {
-                items: 3
-            },
-            1170: {
-                items: 4
-            }
-        }
+      },
+      1170: {
+        items: 4,
+      },
+    },
+  });
 
-    });
+  $('.product-carousel').owlCarousel({
+    loop: true,
+    items: 4,
+    margin: 15,
+    autoplay: false,
+    dots: false,
+    autoplayTimeout: 8000,
+    rewindSpeed: 8000,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      990: {
+        items: 3,
+      },
+      1170: {
+        items: 4,
+      },
+    },
+  });
 
-    $(".vertical-spin").TouchSpin({
-        verticalbuttons: true,
-        verticalupclass: 'fa fa-plus',
-        verticaldownclass: 'fa fa-minus',
-        max: 50
-    });
-
+  $('.vertical-spin').TouchSpin({
+    verticalbuttons: true,
+    verticalupclass: 'fa fa-plus',
+    verticaldownclass: 'fa fa-minus',
+    max: 50,
+  });
 });
 
-$(window).scroll(function(){
-    const backtoTop= document.querySelector(".back-to-top");
-  
-  window.addEventListener("scroll",()=>{
-      if(window.scrollY>100 ){
-          backtoTop.classList.add("active");
-      }else{
-          backtoTop.classList.remove("active");
-      }
-  })
+$(window).scroll(function () {
+  const backtoTop = document.querySelector('.back-to-top');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      backtoTop.classList.add('active');
+    } else {
+      backtoTop.classList.remove('active');
+    }
   });
+});
